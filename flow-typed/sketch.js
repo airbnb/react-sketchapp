46c6f7c37248617a7521e4676233b0f1538cd37a
Jon Gold <jon.gold@airbnb.com>
2016-12-07 13:25:01 -0800

    Add CGAffineTransformation* to flow-typed

--- flow-typed/sketch.js
+++ flow-typed/sketch.js
@@ -2,6 +2,10 @@
 
 declare var log: any;
 declare var COSAlertWindow: any;
+declare var CGAffineTransformConcat: any;
+declare var CGAffineTransformMakeScale: any;
+declare var CGAffineTransformMakeRotation: any;
+declare var CGAffineTransformMakeTranslation: any;
 declare var NSTextField: any;
 declare var NSString: any;
 declare var NSFont: any;
