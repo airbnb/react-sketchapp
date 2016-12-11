6f1cc02d19f0b529c1ef59ea03068cc70445e985
Jon Gold <jon.gold@airbnb.com>
2016-12-11 11:12:00 -0800

    fix imports in example-plugin/StyleGuide

--- example-plugin/Styleguide.js
+++ example-plugin/Styleguide.js
@@ -3,31 +3,31 @@
 import React from 'react';
 import { render, View } from '../src';
 import designSystem from './designSystem';
+import type { DesignSystem } from './designSystem';
 
 import Palette from './components/Palette';
 import TypeSpecimen from './components/TypeSpecimen';
 import Section from './components/Section';
 
-const Document = ({ fonts, colors }: any) =>
+const Document = ({ system }: { system: DesignSystem }) =>
   <View>
     <Section title="Type Styles">
       <View>
-        { Object.keys(fonts).map(name =>
-          <TypeSpecimen name={name} style={fonts[name]} />)
+        { Object.keys(system.fonts).map(name =>
+          <TypeSpecimen name={name} style={system.fonts[name]} />)
         }
       </View>
     </Section>
 
     <Section title="Color">
-      <Palette colors={colors} />
+      <Palette colors={system.colors} />
     </Section>
   </View>;
 
 const onRun = (context: any) => {
   render(
     <Document
-      fonts={designSystem.fonts}
-      colors={designSystem.colors}
+      system={designSystem}
     />
   , context);
 };
