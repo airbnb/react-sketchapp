bb82c7681ca94ef290e8f19bb1ff150b5ca62e4b
Jon Gold <jon.gold@airbnb.com>
2017-01-23 15:19:02 -0800

    remove cached layerTextStyles from TextStyles class

--- src/TextStyles.js
+++ src/TextStyles.js
@@ -1,34 +1,36 @@
 /* @flow */
 import invariant from 'invariant';
-import type { SketchContext, SketchSharedStyleContainer, SketchStyle } from './types';
+import type { SketchContext, SketchStyle } from './types';
 
 class TextStyles {
-  _context: any;
-  _layerTextStyles: SketchSharedStyleContainer;
+  _context: ?SketchContext;
 
-  constructor(context?: SketchContext) {
-    if (context) { this.setContext(context); }
+  constructor() {
+    this._context = null;
   }
 
   setContext(context: SketchContext) {
     invariant(context, 'Please provide a context');
 
     this._context = context;
-    this._layerTextStyles = context.document.documentData().layerTextStyles();
     return this;
   }
 
   setStyles(styles: Array<any>) {
-    invariant(this._layerTextStyles, 'Please provide a context');
+    invariant(this._context, 'Please provide a context');
 
-    this._layerTextStyles.setObjects(styles);
+    this._context.document.documentData().layerTextStyles()
+      .setObjects(styles);
+
     return this;
   }
 
   addStyle(name: string, style: SketchStyle) {
-    invariant(this._layerTextStyles, 'Please provide a context');
+    invariant(this._context, 'Please provide a context');
 
-    this._layerTextStyles.addSharedStyleWithName_firstInstance(name, style);
+    this._context.document.documentData().layerTextStyles()
+      .addSharedStyleWithName_firstInstance(name, style);
+
     return this;
   }
 }
