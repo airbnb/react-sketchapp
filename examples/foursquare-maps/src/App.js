280d06a7b0bacaee70c874d4ef77caeddafc9cc4
Jon Gold <hello@designedbygold.com>
2017-04-05 22:30:34 -0700

    fix proptypes in foursquare-maps

--- examples/foursquare-maps/src/App.js
+++ examples/foursquare-maps/src/App.js
@@ -40,17 +40,17 @@ const styles = StyleSheet.create({
   },
 });
 
-const LatLong = {
+const LatLong = PropTypes.shape({
   latitude: PropTypes.string,
   longitude: PropTypes.string,
-};
+});
 
-const Venue = PropTypes.shape({
+const Venue = {
   name: PropTypes.string,
   location: PropTypes.shape({
     address: PropTypes.string,
   }),
-});
+};
 
 const Row = ({ name, location }) => (
   <View style={styles.rowWrapper} name={name}>
@@ -85,7 +85,7 @@ const App = ({ center, venues }) => {
 };
 App.propTypes = {
   center: LatLong,
-  venues: PropTypes.arrayOf(Venue),
+  venues: PropTypes.arrayOf(PropTypes.shape(Venue)),
 };
 
 export default App;
