7433842f0dae45708650c2773f27af9f785c0a09
Lloyd Wheeler <lloydwheeler@gmail.com>
2017-05-01 12:04:31 +0100

    create design system

---
+++ examples/basic-form/src/designSystem.js
@@ -0,0 +1,38 @@
+export const colors = {
+  Yellow: '#f9cc00',
+  Orange: '#fd6134',
+  Rose: '#ff4289',
+  Green: '#005b4c',
+  Black: '#222223',
+  LightGrey: '#eeeeee'
+};
+
+export const spacing = {
+  xSmall: 4,
+  Small: 8,
+  Medium: 16,
+  Large: 32,
+  xLarge: 64
+};
+
+// http://www.modularscale.com/?16&px&1.5
+export const typeRamp = {
+  xSmall: 7,
+  Small: 10,
+  Medium: 16,
+  Large: 24,
+  xLarge: 36
+};
+
+export const typography = {
+  Heading: {
+    fontSize: typeRamp.Large
+  }
+}
+
+export default {
+  colors,
+  spacing,
+  typeRamp,
+  typography
+}
