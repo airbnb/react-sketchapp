65add31213f7e8f6fc90ff4ad2d9834fa3b74cab
David E. Chen <dchen@users.noreply.github.com>
2017-05-01 14:41:09 -0700

    Support for opacity (#60)
    * Support for opacity style
    
    * Add test for opacity
    
    * Remove TODO comment for opacity

--- src/renderers/SketchRenderer.js
+++ src/renderers/SketchRenderer.js
@@ -3,6 +3,8 @@ import layerGroup from '../jsonUtils/layerGroup';
 
 import type { LayoutInfo, ViewStyle, TextStyle, SketchJSON } from '../types';
 
+const DEFAULT_OPACITY = 1.0;
+
 class SketchRenderer {
   getDefaultGroupName(/* props: any, value: ?string */) {
     return 'Group';
@@ -13,7 +15,7 @@ class SketchRenderer {
     textStyle: TextStyle,
     props: any,
     // eslint-disable-next-line no-unused-vars
-    value: ?string,
+    value: ?string
   ): SketchJSON {
     // Default SketchRenderer just renders an empty group
 
@@ -22,13 +24,10 @@ class SketchRenderer {
     //   processTransform(layer, layout, style.transform);
     // }
 
-    // TODO(akp): handle opacity #sketch43
-    // if (style.opacity !== undefined) {
-    //   layer.style().contextSettings().opacity = style.opacity;
-    // }
+    const opacity = style.opacity || DEFAULT_OPACITY;
 
     return {
-      ...layerGroup(layout.left, layout.top, layout.width, layout.height),
+      ...layerGroup(layout.left, layout.top, layout.width, layout.height, opacity),
       name: props.name || this.getDefaultGroupName(props, value),
     };
   }
@@ -38,7 +37,7 @@ class SketchRenderer {
     textStyle: TextStyle,
     props: any,
     // eslint-disable-next-line no-unused-vars
-    value: ?string,
+    value: ?string
   ): Array<SketchJSON> {
     return [];
   }
