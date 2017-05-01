7084ab61ee78d5f51648ee06d959b71dd09ed8a0
Lloyd Wheeler <lloydwheeler@gmail.com>
2017-05-01 14:18:12 +0100

    add border-box to button styles

--- examples/basic-form/src/components/Button.js
+++ examples/basic-form/src/components/Button.js
@@ -2,23 +2,15 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-primitives';
 import { spacing, colors } from '../designSystem';
 
-
-const styles = StyleSheet.create({
-  button: {
-    color: colors.White,
-    backgroundColor: colors.Rose,
-    padding: spacing.Medium,
-    textAlign: 'center',
-    width: 260
-  }
-});
-
 const buttonStyle = {
+  borderRadius: 3,
+  boxSizing: 'border-box',
   color: colors.White,
   padding: spacing.Medium,
   textAlign: 'center',
   width: 260
 }
+
 
 const Button = ({ label, backgroundColor }: Props) => (
   <Text style={{...buttonStyle, backgroundColor: backgroundColor}}>
