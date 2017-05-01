dc4e9bfcf0fb51f6da28bd26021698e6ee8d04e2
Lloyd Wheeler <lloydwheeler@gmail.com>
2017-05-01 10:55:17 +0100

    add dummy session data

---
+++ examples/basic-form/src/data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    email: '',
+    password: ''
+  },
+  {
+    email: 'john.hornsby@example.com',
+    password: ''
+  },
+  {
+    email: 'john.hornsby@example.com',
+    password: 'hello'
+  },
+  {
+    email: 'john.hornsby@example.com',
+    password: '!H3ll0!'
+  }
+]
