1a4ca00ffe8b5a6783f595f94c4c6adce277c847
Lloyd Wheeler <lloydwheeler@gmail.com>
2017-05-01 14:14:57 +0100

    add strength meter component

---
+++ examples/basic-form/src/components/StrengthMeter.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-primitives';
+import { colors } from '../designSystem';
+
+const strengths = {
+  short: {
+    width: 65,
+    label: 'Too short',
+    backgroundColor: colors.Rose
+  },
+  fair: {
+    width: 130,
+    label: 'Fair',
+    backgroundColor: colors.Yellow
+  },
+  good: {
+    width: 195,
+    label: 'Good',
+    backgroundColor: colors.Yellow
+  },
+  strong: {
+    width: 260,
+    label: 'Strong',
+    backgroundColor: colors.Green
+  }
+}
+
+const styles = StyleSheet.create({
+  meter: {
+    boxSizing: 'border-box',
+    height: 5,
+    width: 260,
+    boxSizing: 'border-box',
+    backgroundColor: '#ddd',
+    marginTop: 10,
+    marginBottom: 40,
+    borderRadius: 5
+  },
+  innerMeter: {
+    height: 5,
+    width: 100,
+    backgroundColor: 'yellow',
+    borderRadius: 5
+  },
+  meterLabel: {
+    textAlign: 'right',
+    width: 260,
+    fontSize: 13,
+    marginTop: 5
+  }
+});
+
+const passwordStrength = (password) => {
+  if(password.length <= 5) {
+    return 'short';
+  } else if (password.length <= 10) {
+    return 'fair';
+  } else if (password.length <= 12) {
+    return 'good';
+  }
+
+  return 'strong';
+}
+
+const StrengthMeter = ({ password }: Props) => (
+  <View style={styles.meter}>
+    <View 
+      style={{
+        boxSizing: 'border-box',
+        height: 5,
+        width: strengths[passwordStrength(password)].width,
+        backgroundColor: strengths[passwordStrength(password)].backgroundColor,
+        borderRadius: 5
+      }}>
+    </View>
+    <Text style={{
+      textAlign: 'right',
+      width: 260,
+      fontSize: 13,
+      marginTop: 5,
+      fontFamily: 'GT Walsheim',
+      color: strengths[passwordStrength(password)].backgroundColor,
+    }}>{strengths[passwordStrength(password)].label}</Text>
+  </View>
+);
+
+
+export default StrengthMeter;
