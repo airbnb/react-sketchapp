077b1f71610724e3b590ff992e2993b79ba13c79
James Baxley <james@meteor.com>
2017-06-29 17:56:11 -0400

    switch to apollo for graphql (#127)
    * switch to apollo
    
    * remove fetch in package.json
    
    * remove polyfill in main file

--- examples/profile-cards-graphql/src/main.js
+++ examples/profile-cards-graphql/src/main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render, Text, View } from 'react-sketchapp';
-import Client from 'gql-sketch';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { graphql, ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
 import type { User } from './types';
 import { fonts, spacing } from './designSystem';
 import Profile from './components/Profile';
@@ -8,27 +10,12 @@ import Space from './components/Space';
 
 const GRAPHQL_ENDPOINT = 'https://api.graph.cool/simple/v1/cj09zm1k4jcpc0115ecsoc1k4';
 
-const Page = ({ users }: { users: Array<User> }) => (
-  <View>
-    <Text style={fonts['Title 1']}>Profile Cards w/ GraphQL</Text>
-    <View
-      style={{
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        width: users.length * 300,
-      }}
-    >
-      {users.map(user => (
-        <Space key={user.screen_name} h={spacing} v={spacing}>
-          <Profile user={user} />
-        </Space>
-      ))}
-    </View>
-  </View>
-);
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({ uri: GRAPHQL_ENDPOINT })
+});
 
-export default (context) => {
-  const QUERY = `{
+const QUERY = gql`
+  {
     allProfiles {
       screenname,
       name,
@@ -37,12 +24,43 @@ export default (context) => {
       url,
       profileImageUrl,
     }
-  }`;
+  }
+`;
 
-  Client(GRAPHQL_ENDPOINT)
-    .query(QUERY)
-    .then(({ allProfiles }) => {
-      render(<Page users={allProfiles} />, context.document.currentPage());
-    })
+const props = ({ data }) => data.loading ? { users: [] } : { users: data.allProfiles };
+const withUsers = graphql(QUERY, { props });
+
+const Page = ({ users }: { users: Array<User> }) => (
+  <View>
+    <Text style={fonts['Title 1']}>Profile Cards w/ GraphQL</Text>
+    {users && (
+      <View
+        style={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          width: users.length * 300,
+        }}
+      >
+        {users.map(user => (
+          <Space key={user.screen_name} h={spacing} v={spacing}>
+            <Profile user={user} />
+          </Space>
+        ))}
+      </View>
+    )}
+  </View>
+);
+
+const PageWithUsers = withUsers(Page);
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <PageWithUsers />
+  </ApolloProvider>
+);
+
+export default (context) => {
+  getDataFromTree(App)
+    .then(() => render(<App />, context.document.currentPage()))
     .catch(console.log);
 }
