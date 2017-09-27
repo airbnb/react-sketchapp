9d851b31f5acd2c349a22580803ad44a7d899bca
Jake Larson <j.larson@m1finance.com>
2017-09-27 11:53:36 -0500

    - Review cleanup/fixes (Make sure to allow zero-able properties for margin, padding, etc) - Version lock yoga-layout for now

--- src/jsonUtils/computeNode.js
+++ src/jsonUtils/computeNode.js
@@ -22,6 +22,8 @@ const isPercent = (value: number | string) => /%/.test(String(value));
 
 const isAuto = (value: number | string) => String(value) === 'auto';
 
+const isNullOrUndefined = (value: any) => value === null || value === undefined;
+
 const computeNode = (node: TreeNode, context: Context) => {
   const yogaNode = yoga.Node.create();
   const hasStyle = node.props && node.props.style;
@@ -31,7 +33,7 @@ const computeNode = (node: TreeNode, context: Context) => {
     // http://facebook.github.io/react-native/releases/0.48/docs/layout-props.html
 
     // Width
-    if (style.width) {
+    if (!isNullOrUndefined(style.width)) {
       if (isPercent(style.width)) {
         yogaNode.setWidthPercent(style.width);
       } else if (isAuto(style.width)) {
@@ -42,7 +44,7 @@ const computeNode = (node: TreeNode, context: Context) => {
     }
 
     // Height
-    if (style.height) {
+    if (!isNullOrUndefined(style.height)) {
       if (isPercent(style.height)) {
         yogaNode.setHeightPercent(style.height);
       } else if (isAuto(style.height)) {
@@ -53,7 +55,7 @@ const computeNode = (node: TreeNode, context: Context) => {
     }
 
     // Min-Height
-    if (style.minHeight) {
+    if (!isNullOrUndefined(style.minHeight)) {
       if (isPercent(style.minHeight)) {
         yogaNode.setMinHeightPercent(style.minHeight);
       } else {
@@ -62,7 +64,7 @@ const computeNode = (node: TreeNode, context: Context) => {
     }
 
     // Min-Width
-    if (style.minWidth) {
+    if (!isNullOrUndefined(style.minWidth)) {
       if (isPercent(style.minWidth)) {
         yogaNode.setMinWidthPercent(style.minWidth);
       } else {
@@ -71,7 +73,7 @@ const computeNode = (node: TreeNode, context: Context) => {
     }
 
     // Max-Height
-    if (style.maxHeight) {
+    if (!isNullOrUndefined(style.maxHeight)) {
       if (isPercent(style.maxHeight)) {
         yogaNode.setMaxHeightPercent(style.maxHeight);
       } else {
@@ -80,7 +82,7 @@ const computeNode = (node: TreeNode, context: Context) => {
     }
 
     // Min-Width
-    if (style.maxWidth) {
+    if (!isNullOrUndefined(style.maxWidth)) {
       if (isPercent(style.maxWidth)) {
         yogaNode.setMaxWidthPercent(style.maxWidth);
       } else {
@@ -89,7 +91,7 @@ const computeNode = (node: TreeNode, context: Context) => {
     }
 
     // Margin
-    if (style.marginTop) {
+    if (!isNullOrUndefined(style.marginTop)) {
       if (isPercent(style.marginTop)) {
         yogaNode.setMarginPercent(yoga.EDGE_TOP, style.marginTop);
       } else if (isAuto(style.marginTop)) {
@@ -98,7 +100,7 @@ const computeNode = (node: TreeNode, context: Context) => {
         yogaNode.setMargin(yoga.EDGE_TOP, style.marginTop);
       }
     }
-    if (style.marginBottom) {
+    if (!isNullOrUndefined(style.marginBottom)) {
       if (isPercent(style.marginBottom)) {
         yogaNode.setMarginPercent(yoga.EDGE_BOTTOM, style.marginBottom);
       } else if (isAuto(style.marginBottom)) {
@@ -107,7 +109,7 @@ const computeNode = (node: TreeNode, context: Context) => {
         yogaNode.setMargin(yoga.EDGE_BOTTOM, style.marginBottom);
       }
     }
-    if (style.marginLeft) {
+    if (!isNullOrUndefined(style.marginLeft)) {
       if (isPercent(style.marginLeft)) {
         yogaNode.setMarginPercent(yoga.EDGE_LEFT, style.marginLeft);
       } else if (isAuto(style.marginLeft)) {
@@ -116,7 +118,7 @@ const computeNode = (node: TreeNode, context: Context) => {
         yogaNode.setMargin(yoga.EDGE_LEFT, style.marginLeft);
       }
     }
-    if (style.marginRight) {
+    if (!isNullOrUndefined(style.marginRight)) {
       if (isPercent(style.marginRight)) {
         yogaNode.setMarginPercent(yoga.EDGE_RIGHT, style.marginRight);
       } else if (isAuto(style.marginRight)) {
@@ -125,7 +127,7 @@ const computeNode = (node: TreeNode, context: Context) => {
         yogaNode.setMargin(yoga.EDGE_RIGHT, style.marginRight);
       }
     }
-    if (style.marginVertical) {
+    if (!isNullOrUndefined(style.marginVertical)) {
       if (isPercent(style.marginVertical)) {
         yogaNode.setMarginPercent(yoga.EDGE_VERTICAL, style.marginVertical);
       } else if (isAuto(style.marginVertical)) {
@@ -134,7 +136,7 @@ const computeNode = (node: TreeNode, context: Context) => {
         yogaNode.setMargin(yoga.EDGE_VERTICAL, style.marginVertical);
       }
     }
-    if (style.marginHorizontal) {
+    if (!isNullOrUndefined(style.marginHorizontal)) {
       if (isPercent(style.marginHorizontal)) {
         yogaNode.setMarginPercent(yoga.EDGE_HORIZONTAL, style.marginHorizontal);
       } else if (isAuto(style.marginHorizontal)) {
@@ -143,7 +145,7 @@ const computeNode = (node: TreeNode, context: Context) => {
         yogaNode.setMargin(yoga.EDGE_HORIZONTAL, style.marginHorizontal);
       }
     }
-    if (style.margin) {
+    if (!isNullOrUndefined(style.margin)) {
       if (isPercent(style.margin)) {
         yogaNode.setMarginPercent(yoga.EDGE_ALL, style.margin);
       } else if (isAuto(style.margin)) {
@@ -154,42 +156,42 @@ const computeNode = (node: TreeNode, context: Context) => {
     }
 
     // Padding
-    if (style.paddingTop) {
+    if (!isNullOrUndefined(style.paddingTop)) {
       if (isPercent(style.paddingTop)) {
         yogaNode.setPaddingPercent(yoga.EDGE_TOP, style.paddingTop);
       } else {
         yogaNode.setPadding(yoga.EDGE_TOP, style.paddingTop);
       }
     }
-    if (style.paddingBottom) {
+    if (!isNullOrUndefined(style.paddingBottom)) {
       if (isPercent(style.paddingBottom)) {
         yogaNode.setPaddingPercent(yoga.EDGE_BOTTOM, style.paddingBottom);
       } else {
         yogaNode.setPadding(yoga.EDGE_BOTTOM, style.paddingBottom);
       }
     }
-    if (style.paddingLeft) {
+    if (!isNullOrUndefined(style.paddingLeft)) {
       if (isPercent(style.paddingLeft)) {
         yogaNode.setPaddingPercent(yoga.EDGE_LEFT, style.paddingLeft);
       } else {
         yogaNode.setPadding(yoga.EDGE_LEFT, style.paddingLeft);
       }
     }
-    if (style.paddingRight) {
+    if (!isNullOrUndefined(style.paddingRight)) {
       if (isPercent(style.paddingRight)) {
         yogaNode.setPaddingPercent(yoga.EDGE_RIGHT, style.paddingRight);
       } else {
         yogaNode.setPadding(yoga.EDGE_RIGHT, style.paddingRight);
       }
     }
-    if (style.paddingVertical) {
+    if (!isNullOrUndefined(style.paddingVertical)) {
       if (isPercent(style.paddingVertical)) {
         yogaNode.setPaddingPercent(yoga.EDGE_VERTICAL, style.paddingVertical);
       } else {
         yogaNode.setPadding(yoga.EDGE_VERTICAL, style.paddingVertical);
       }
     }
-    if (style.paddingHorizontal) {
+    if (!isNullOrUndefined(style.paddingHorizontal)) {
       if (isPercent(style.paddingHorizontal)) {
         yogaNode.setPaddingPercent(
           yoga.EDGE_HORIZONTAL,
@@ -199,7 +201,7 @@ const computeNode = (node: TreeNode, context: Context) => {
         yogaNode.setPadding(yoga.EDGE_HORIZONTAL, style.paddingHorizontal);
       }
     }
-    if (style.padding) {
+    if (!isNullOrUndefined(style.padding)) {
       if (isPercent(style.padding)) {
         yogaNode.setPaddingPercent(yoga.EDGE_ALL, style.padding);
       } else {
@@ -208,39 +210,39 @@ const computeNode = (node: TreeNode, context: Context) => {
     }
 
     // Border
-    if (style.borderTop) {
+    if (!isNullOrUndefined(style.borderTop)) {
       yogaNode.setBorder(yoga.EDGE_TOP, style.borderTop);
     }
-    if (style.borderBottom) {
+    if (!isNullOrUndefined(style.borderBottom)) {
       yogaNode.setBorder(yoga.EDGE_BOTTOM, style.borderBottom);
     }
-    if (style.borderLeft) {
+    if (!isNullOrUndefined(style.borderLeft)) {
       yogaNode.setBorder(yoga.EDGE_LEFT, style.borderLeft);
     }
-    if (style.borderRight) {
+    if (!isNullOrUndefined(style.borderRight)) {
       yogaNode.setBorder(yoga.EDGE_RIGHT, style.borderRight);
     }
-    if (style.borderVertical) {
+    if (!isNullOrUndefined(style.borderVertical)) {
       yogaNode.setBorder(yoga.EDGE_VERTICAL, style.borderVertical);
     }
-    if (style.borderHorizontal) {
+    if (!isNullOrUndefined(style.borderHorizontal)) {
       yogaNode.setBorder(yoga.EDGE_HORIZONTAL, style.borderHorizontal);
     }
-    if (style.border) {
+    if (!isNullOrUndefined(style.border)) {
       yogaNode.setBorder(yoga.EDGE_ALL, style.border);
     }
 
     // Flex
-    if (style.flex) {
+    if (!isNullOrUndefined(style.flex)) {
       yogaNode.setFlex(style.flex);
     }
-    if (style.flexGrow) {
+    if (!isNullOrUndefined(style.flexGrow)) {
       yogaNode.setFlexGrow(style.flexGrow);
     }
-    if (style.flexShrink) {
+    if (!isNullOrUndefined(style.flexShrink)) {
       yogaNode.setFlexShrink(style.flexShrink);
     }
-    if (style.flexBasis) {
+    if (!isNullOrUndefined(style.flexBasis)) {
       yogaNode.setFlexBasis(style.flexBasis);
     }
 
@@ -252,28 +254,28 @@ const computeNode = (node: TreeNode, context: Context) => {
       yogaNode.setPositionType(yoga.POSITION_TYPE_RELATIVE);
     }
 
-    if (style.top) {
+    if (!isNullOrUndefined(style.top)) {
       if (isPercent(style.top)) {
         yogaNode.setPositionPercent(yoga.EDGE_TOP, style.top);
       } else {
         yogaNode.setPosition(yoga.EDGE_TOP, style.top);
       }
     }
-    if (style.left) {
+    if (!isNullOrUndefined(style.left)) {
       if (isPercent(style.left)) {
         yogaNode.setPositionPercent(yoga.EDGE_LEFT, style.left);
       } else {
         yogaNode.setPosition(yoga.EDGE_LEFT, style.left);
       }
     }
-    if (style.right) {
+    if (!isNullOrUndefined(style.right)) {
       if (isPercent(style.right)) {
         yogaNode.setPositionPercent(yoga.EDGE_RIGHT, style.right);
       } else {
         yogaNode.setPosition(yoga.EDGE_RIGHT, style.right);
       }
     }
-    if (style.bottom) {
+    if (!isNullOrUndefined(style.bottom)) {
       if (isPercent(style.bottom)) {
         yogaNode.setPositionPercent(yoga.EDGE_BOTTOM, style.bottom);
       } else {
@@ -282,146 +284,138 @@ const computeNode = (node: TreeNode, context: Context) => {
     }
 
     // Display
-    const display = style.display;
-    if (display) {
-      if (display === 'flex') {
+    if (style.display) {
+      if (style.display === 'flex') {
         yogaNode.setDisplay(yoga.DISPLAY_FLEX);
       }
-      if (display === 'none') {
+      if (style.display === 'none') {
         yogaNode.setDisplay(yoga.DISPLAY_NONE);
       }
     }
 
     // Overflow
-    const overflow = style.overflow;
-    if (overflow) {
-      if (overflow === 'visible') {
+    if (style.overflow) {
+      if (style.overflow === 'visible') {
         yogaNode.setDisplay(yoga.OVERFLOW_VISIBLE);
       }
-      if (overflow === 'scroll') {
+      if (style.overflow === 'scroll') {
         yogaNode.setDisplay(yoga.OVERFLOW_SCROLL);
       }
-      if (overflow === 'hidden') {
+      if (style.overflow === 'hidden') {
         yogaNode.setDisplay(yoga.OVERFLOW_HIDDEN);
       }
     }
 
     // Flex direction
-    const flexDirection = style.flexDirection;
-    if (flexDirection) {
-      if (flexDirection === 'row') {
+    if (style.flexDirection) {
+      if (style.flexDirection === 'row') {
         yogaNode.setFlexDirection(yoga.FLEX_DIRECTION_ROW);
       }
-      if (flexDirection === 'column') {
+      if (style.flexDirection === 'column') {
         yogaNode.setFlexDirection(yoga.FLEX_DIRECTION_COLUMN);
       }
-      if (flexDirection === 'row-reverse') {
+      if (style.flexDirection === 'row-reverse') {
         yogaNode.setFlexDirection(yoga.FLEX_DIRECTION_ROW_REVERSE);
       }
-      if (flexDirection === 'column-reverse') {
+      if (style.flexDirection === 'column-reverse') {
         yogaNode.setFlexDirection(yoga.FLEX_DIRECTION_COLUMN_REVERSE);
       }
     }
 
     // Justify Content
-    const justifyContent = style.justifyContent;
-    if (justifyContent) {
-      if (justifyContent === 'flex-start') {
+    if (style.justifyContent) {
+      if (style.justifyContent === 'flex-start') {
         yogaNode.setJustifyContent(yoga.JUSTIFY_FLEX_START);
       }
-      if (justifyContent === 'flex-end') {
+      if (style.justifyContent === 'flex-end') {
         yogaNode.setJustifyContent(yoga.JUSTIFY_FLEX_END);
       }
-      if (justifyContent === 'center') {
+      if (style.justifyContent === 'center') {
         yogaNode.setJustifyContent(yoga.JUSTIFY_CENTER);
       }
-      if (justifyContent === 'space-between') {
+      if (style.justifyContent === 'space-between') {
         yogaNode.setJustifyContent(yoga.JUSTIFY_SPACE_BETWEEN);
       }
-      if (justifyContent === 'space-around') {
+      if (style.justifyContent === 'space-around') {
         yogaNode.setJustifyContent(yoga.JUSTIFY_SPACE_AROUND);
       }
     }
 
     // Align Content
-    const alignContent = style.alignContent;
-    if (alignContent) {
-      if (alignContent === 'flex-start') {
+    if (style.alignContent) {
+      if (style.alignContent === 'flex-start') {
         yogaNode.setAlignContent(yoga.ALIGN_FLEX_START);
       }
-      if (alignContent === 'flex-end') {
+      if (style.alignContent === 'flex-end') {
         yogaNode.setAlignContent(yoga.ALIGN_FLEX_END);
       }
-      if (alignContent === 'center') {
+      if (style.alignContent === 'center') {
         yogaNode.setAlignContent(yoga.ALIGN_CENTER);
       }
-      if (alignContent === 'stretch') {
+      if (style.alignContent === 'stretch') {
         yogaNode.setAlignContent(yoga.ALIGN_STRETCH);
       }
-      if (alignContent === 'baseline') {
+      if (style.alignContent === 'baseline') {
         yogaNode.setAlignContent(yoga.ALIGN_BASELINE);
       }
-      if (alignContent === 'space-between') {
+      if (style.alignContent === 'space-between') {
         yogaNode.setAlignContent(yoga.ALIGN_SPACE_BETWEEN);
       }
-      if (alignContent === 'space-around') {
+      if (style.alignContent === 'space-around') {
         yogaNode.setAlignContent(yoga.ALIGN_SPACE_AROUND);
       }
-      if (alignContent === 'auto') {
+      if (style.alignContent === 'auto') {
         yogaNode.setAlignContent(yoga.ALIGN_AUTO);
       }
     }
 
     // Align Items
-    const alignItems = style.alignItems;
-    if (alignItems) {
-      if (alignItems === 'flex-start') {
+    if (style.alignItems) {
+      if (style.alignItems === 'flex-start') {
         yogaNode.setAlignItems(yoga.ALIGN_FLEX_START);
       }
-      if (alignItems === 'flex-end') {
+      if (style.alignItems === 'flex-end') {
         yogaNode.setAlignItems(yoga.ALIGN_FLEX_END);
       }
-      if (alignItems === 'center') {
+      if (style.alignItems === 'center') {
         yogaNode.setAlignItems(yoga.ALIGN_CENTER);
       }
-      if (alignItems === 'stretch') {
+      if (style.alignItems === 'stretch') {
         yogaNode.setAlignItems(yoga.ALIGN_STRETCH);
       }
-      if (alignItems === 'baseline') {
+      if (style.alignItems === 'baseline') {
         yogaNode.setAlignItems(yoga.ALIGN_BASELINE);
       }
     }
 
     // Align Self
-    const alignSelf = style.alignSelf;
-    if (alignSelf) {
-      if (alignSelf === 'flex-start') {
+    if (style.alignSelf) {
+      if (style.alignSelf === 'flex-start') {
         yogaNode.setAlignSelf(yoga.ALIGN_FLEX_END);
       }
-      if (alignSelf === 'flex-end') {
+      if (style.alignSelf === 'flex-end') {
         yogaNode.setAlignSelf(yoga.ALIGN_FLEX_END);
       }
-      if (alignSelf === 'center') {
+      if (style.alignSelf === 'center') {
         yogaNode.setAlignSelf(yoga.ALIGN_CENTER);
       }
-      if (alignSelf === 'stretch') {
+      if (style.alignSelf === 'stretch') {
         yogaNode.setAlignSelf(yoga.ALIGN_STRETCH);
       }
-      if (alignSelf === 'baseline') {
+      if (style.alignSelf === 'baseline') {
         yogaNode.setAlignSelf(yoga.ALIGN_BASELINE);
       }
     }
 
     // Flex Wrap
-    const flexWrap = style.flexWrap;
-    if (flexWrap) {
-      if (flexWrap === 'nowrap') {
+    if (style.flexWrap) {
+      if (style.flexWrap === 'nowrap') {
         yogaNode.setFlexWrap(yoga.WRAP_NO_WRAP);
       }
-      if (flexWrap === 'wrap') {
+      if (style.flexWrap === 'wrap') {
         yogaNode.setFlexWrap(yoga.WRAP_WRAP);
       }
-      if (flexWrap === 'wrap-reverse') {
+      if (style.flexWrap === 'wrap-reverse') {
         yogaNode.setFlexWrap(yoga.WRAP_WRAP_REVERSE);
       }
     }
