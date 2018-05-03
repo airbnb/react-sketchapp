184da6f5989dc0de106c9dd3b60a76defb53c183
Mathieu Dutour <mat.dutour@gmail.com>
2018-05-03 16:55:56 +0200

    set NSColor for Sketch < 50

--- src/jsonUtils/hacksForJSONImpl.js
+++ src/jsonUtils/hacksForJSONImpl.js
@@ -225,26 +225,26 @@ export function makeResizeConstraint(
 function createStringAttributes(textStyles: TextStyle): Object {
   const font = findFont(textStyles);
 
-  const color = makeColorFromCSS(textStyles.color || 'black');
-
-  const colorAttribute =
-    getSketchVersion() >= 50
-      ? color
-      : NSColor.colorWithDeviceRed_green_blue_alpha(
-          color.red,
-          color.green,
-          color.blue,
-          color.alpha
-        );
-
   const attribs: Object = {
     MSAttributedStringFontAttribute: font.fontDescriptor(),
     NSFont: font,
     NSParagraphStyle: makeParagraphStyle(textStyles),
-    MSAttributedStringColorAttribute: colorAttribute,
     NSUnderline: TEXT_DECORATION_UNDERLINE[textStyles.textDecoration] || 0,
     NSStrikethrough: TEXT_DECORATION_LINETHROUGH[textStyles.textDecoration] || 0,
   };
+  
+  const color = makeColorFromCSS(textStyles.color || 'black');
+
+  if (getSketchVersion() >= 50) {
+    attribs.MSAttributedStringColorAttribute = color
+  } else {
+    attribs.NSColor = NSColor.colorWithDeviceRed_green_blue_alpha(
+      color.red,
+      color.green,
+      color.blue,
+      color.alpha
+    );
+  }
 
   if (textStyles.letterSpacing !== undefined) {
     attribs.NSKern = textStyles.letterSpacing;
