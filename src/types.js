bec7be160a912ea31efed688ef5f773b8a73cb99
Thierry Charbonnel <thierryc@users.noreply.github.com>
2018-06-11 16:02:04 -0400

    multi shadows support

--- src/types.js
+++ src/types.js
@@ -84,6 +84,7 @@ export type ViewStyle = {
   shadowOffset?: { width: number, height: number },
   shadowOpacity?: number,
   shadowRadius?: number,
+  shadowInner?: boolean,
   width?: number,
   height?: number,
   top?: number,
@@ -198,3 +199,14 @@ export type ResizeConstraints = {
   fixedHeight: boolean,
   fixedWidth: boolean,
 };
+
+export type SketchShadow = {
+  shadowColor: Color,
+  shadowOffset: { width: number, height: number },
+  shadowSpread: number,
+  shadowOpacity: number,
+  shadowRadius: number,
+  shadowInner: boolean,
+};
+
+export type SketchShadows = Array<SketchShadow>;
