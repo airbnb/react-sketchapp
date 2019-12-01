47a1b380cc4795455f71cac5dc6522510a3e81cf
Mathieu Dutour <mathieu@dutour.me>
2019-12-01 22:24:24 +0100

    add proper types for js-sha

--- src/types/js-sha1.d.ts
+++ src/types/js-sha1.d.ts
@@ -1,1 +1,13 @@
-declare module 'js-sha1';
+declare module 'js-sha1' {
+  function sha1<T>(dest: string | Array<T> | ArrayBuffer | Uint8Array): string;
+
+  namespace sha1 {
+    export function hex(arg: string): string;
+    export function update<T>(arg: string | Array<T> | Uint8Array | ArrayBuffer): string;
+    export function array(): Uint8Array;
+    export function digest(): Uint8Array;
+    export function arrayBuffer(): ArrayBuffer;
+  }
+
+  export = sha1;
+}
