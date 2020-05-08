bed0701908576de95a88bc3a11887df84e9fd1a8
macintoshhelper <macintoshhelper@users.noreply.github.com>
2020-05-08 23:04:00 +0100

    add LOCAL_DEV conditional Webpack module aliasing to examples

---
+++ examples/form-validation/webpack.skpm.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = (config) => {
+  if (process.env.LOCAL_DEV) {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        'react-sketchapp': path.resolve(__dirname, '../../lib/'),
+      },
+    };
+  }
+};
