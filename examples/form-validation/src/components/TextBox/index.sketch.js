2c118113f6560b9a4baed39fffd0e52bf002b278
macintoshhelper <macintoshhelper@users.noreply.github.com>
2020-05-10 01:16:52 +0100

    remove flow types from form validation TextBox component

--- examples/form-validation/src/components/TextBox/index.sketch.js
+++ examples/form-validation/src/components/TextBox/index.sketch.js
@@ -2,13 +2,7 @@ import * as React from 'react';
 import { View, Text } from 'react-primitives';
 import styles from './style';
 
-type Props = {
-  label: string,
-  value: string,
-  children?: React$Element<any>,
-};
-
-const TextBox = ({ label, value, children }: Props) => (
+const TextBox = ({ label, value, children }) => (
   <View style={styles.formElement}>
     <Text style={styles.label}>{label}</Text>
     <View style={styles.textbox}>
