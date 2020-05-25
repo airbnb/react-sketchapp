29779eb7788e7bff5941cba4368cacca4f7bb3dc
macintoshhelper <macintoshhelper@users.noreply.github.com>
2020-05-25 17:09:02 +0100

    migrate skpm webpack.config to __tests__/examples

--- __tests__/examples/webpack.skpm.config.js
+++ __tests__/examples/webpack.skpm.config.js
@@ -1,19 +1,15 @@
 const path = require('path');
 
-const aliasedModules = [
-  'chroma-js',
-  'ramda',
-  'react-primitives',
-  '@emotion/primitives',
-  '@emotion/core',
-];
+const pkg = require(path.resolve(__dirname, 'package.json'));
+
+const aliasedModules = Object.keys(pkg.dependencies);
 
 module.exports = (config) => {
   config.resolve = {
     ...config.resolve,
     alias: {
       ...config.resolve.alias,
-      'react-sketchapp': path.resolve(__dirname, '../../'),
+      'react-sketchapp': path.resolve(__dirname, '../../lib'),
 
       ...aliasedModules.reduce((acc, mod) => {
         acc[mod] = path.resolve(__dirname, `./node_modules/${mod}`);
--- __tests__/skpm/webpack.skpm.config.js
+++ __tests__/skpm/webpack.skpm.config.js
@@ -1,24 +1,3 @@
-const path = require('path');
+const webpackConfig = require('../examples/webpack.skpm.config');
 
-const aliasedModules = [
-  'chroma-js',
-  'ramda',
-  'react-primitives',
-  '@emotion/primitives',
-  '@emotion/core',
-];
-
-module.exports = (config) => {
-  config.resolve = {
-    ...config.resolve,
-    alias: {
-      ...config.resolve.alias,
-      'react-sketchapp': path.resolve(__dirname, '../../lib/index.js'),
-
-      ...aliasedModules.reduce((acc, mod) => {
-        acc[mod] = path.resolve(__dirname, `../examples/node_modules/${mod}`);
-        return acc;
-      }, {}),
-    },
-  };
-};
+module.exports = webpackConfig;
