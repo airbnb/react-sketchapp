fd830e393a509c89c42dd272cbe3a945d5645209
macintoshhelper <macintoshhelper@users.noreply.github.com>
2020-05-25 02:00:54 +0100

    add babel/webpack setup for e2e example tests

---
+++ __tests__/skpm/webpack.skpm.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const aliasedModules = [
+  'chroma-js',
+  'ramda',
+  'react-primitives',
+  '@emotion/primitives',
+  '@emotion/core',
+];
+
+module.exports = (config) => {
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      ...config.resolve.alias,
+      'react-sketchapp': path.resolve(__dirname, '../../lib/index.sketch.js'),
+
+      ...aliasedModules.reduce((acc, mod) => {
+        acc[mod] = path.resolve(__dirname, `../examples/node_modules/${mod}`);
+        return acc;
+      }, {}),
+    },
+  };
+};
